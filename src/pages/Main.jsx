43c0cbf0213f4246a4d9f94570b663cf2fc7e4b5
mttaskin <0mttaskin1@gmail.com>
2023-03-14 21:45:43 +0300

    v9

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import MovieCard from "../components/MovieCard";
+import { MovieContext } from "../context/MovieContext";
 
 const Main = () => {
-  return <div>
-
-  </div>;
+  const { movies } = useContext(MovieContext);
+  return (
+    <div>
+      <div className="flex justify-center flex-wrap">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
